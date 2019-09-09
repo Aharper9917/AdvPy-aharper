ced2789b2ad0a9bf428a34b798b43b77c63f539a
allen <allen.harper9917@gmail.com>
2019-09-09 13:37:23 -0600

    Submitting kattis 1/6

---
+++ kattis/9Knights/9k.py
@@ -0,0 +1,69 @@
+"""
+https://open.kattis.com/problems/nineknights
+"""
+
+
+def test():
+    b1 = [[".", ".", "k", "k", "."], [".", ".", ".", ".", "."], [
+        ".", "k", "k", "k", "."], [".", ".", ".", ".", "."], [".", ".", ".", ".", "."]]
+    b2 = [[".", ".", ".", ".", "."], [".", ".", ".", ".", "."], [
+        ".", ".", ".", ".", "."], [".", ".", ".", ".", "."], [".", ".", ".", ".", "."]]
+    b3 = [[".", ".", ".", ".", "k"], [".", ".", ".", "k", "."], [
+        "k", ".", "k", ".", "."],  [".", "k", ".", "k", "."], ["k", ".", "k", ".", "k"]]
+
+    assert checkValidity(b1) == 'invalid'
+    assert checkValidity(b2) == 'invalid'
+    assert checkValidity(b3) == 'valid'
+
+def readBoard():
+    board = [[".", ".", ".", ".", "."], [".", ".", ".", ".", "."], [".", ".", ".", ".", "."], [".", ".", ".", ".", "."], [".", ".", ".", ".", "."]]
+
+    for i in range(5):
+        x = input()
+        for j in range(5):
+            board[i][j] = x[j]
+
+    return board
+
+def checkValidity(board):
+    count = 0
+    moves = [
+        [-1,  2],
+        [-1, -2],
+        [ 1,  2],
+        [ 1, -2],
+        [ 2, -1],
+        [ 2,  1],
+        [-2, -1],
+        [-2,  1]
+    ]
+    for i in range(5):
+        for j in range(5):
+            if board[i][j] == 'k':
+                count += 1
+
+                # print("Moves for board[%d][%d] 'k' are:" %(i,j))
+
+                for move in moves:
+                    try:
+                        a = i+move[0]
+                        b = j+move[1]
+                        if a >= 0 and b >= 0 and board[a][b] == 'k':
+                            return 'invalid'
+                            # print("board[%d][%d]: " % (i+move[0], j+move[1]) + board[a][b])
+                    except: pass
+    if count != 9:
+        return "invalid"
+    return 'valid'
+
+def main():
+    test()
+    board = readBoard()
+    # for row in board:
+    #     print(row)
+    print(checkValidity(board))
+
+        
+if __name__ == '__main__':
+    main()
+    
